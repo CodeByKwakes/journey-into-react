c65e693821072b8a6f5c2d928c218cb5882e4b6e
KwakesProject <kwakes@me.com>
2016-09-23 16:14:05 +0100

    part 3 done. will refractor once complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,38 @@
+//=================
+// Import the dependencies that this component will need
+// to function properly
+//=================
 import React, { Component } from "react";
 
-/*export default class App extends Component {  
+//=================
+// Here we will import our navigation dependency
+//=================
+import Navigation from './navigation';
+
+//=================
+// This is the component we'll be exporting into the
+// #app element on the index.html file.
+//=================
+export default class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Navigation />
+        {this.props.children}
       </div>
     );
   }
-}*/
+}
 
-class App extends Component {
+/*class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Navigation />
+        {this.props.children}
       </div>
     )
   }
 }
 
-export default App
+export default App*/
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './components/app'
+//==================
+// Here we import the react component again and then
+// the new dependencie reactdom that actually renders
+// the application onto the screen.
+//==================
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
-ReactDom.render( <App />, document.querySelector('#app'))
+//==================
+// Now we replace our App component with the routes component
+// and substitute it into the function below
+//==================
+// import App from './components/app'
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+//==================
+// This command actually renders the component into
+// the element with the id #app which we added in
+// the index.html file. Instead of serving the <app /> like we did previously
+// we now serve the Router we defined in the router.js file. 
+//==================
+// ReactDom.render( <App />, document.querySelector('#app'))
+ReactDOM.render(<Router history={browserHistory} routes={routes} />, document.querySelector('#app'));
