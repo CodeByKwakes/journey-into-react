5f494ede88d70c9b569d9b1144f2172ea54a795a
KwakesProject <kwakes@me.com>
2016-09-23 15:14:45 +0100

    part 2 of tutorial done

--- src/components/app.js
+++ src/components/app.js
@@ -1,1 +1,23 @@
 import React, { Component } from "react";
+
+/*export default class App extends Component {  
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}*/
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default App
